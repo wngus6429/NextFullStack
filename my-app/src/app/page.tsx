c43df6a2db3fc9df7b86993eb288dd5703a01ebf
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2023-04-11 03:00:25 +0900

    update

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,11 +1,20 @@
 // import Image from 'next/image';
 // import { Inter } from '@next/font/google';
 // import styles from './page.module.css';
+import Counter from '@/components/Counter';
 import { notFound } from 'next/navigation';
+import os from 'os'; // 노드 APIS
 
 // const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   // notFound();
-  return <h1>홈페이지</h1>;
+  console.log('안녕 ! 서버');
+  console.log(os.hostname());
+  return (
+    <>
+      <h1>홈페이지</h1>
+      <Counter />
+    </>
+  );
 }
