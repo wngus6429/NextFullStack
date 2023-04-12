00c90595df18bc7dfe916e220d2b08aafccbfe60
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2023-04-12 23:53:56 +0900

    update loading, Error

--- my-app/src/app/products/page.tsx
+++ my-app/src/app/products/page.tsx
@@ -7,6 +7,7 @@ import MeowArticle from '@/components/MeowArticle';
 // export const revalidate = 3;
 
 export default async function ProductsPage() {
+  throw new Error();
   //! 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줄 예정
   const products = await getProducts();
   return (
